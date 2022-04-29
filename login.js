d5def5698dd8ecd3d1063dc05d05f6b025d7885e
jessie0219 <102656884+jessie0219@users.noreply.github.com>
2022-04-29 20:07:51 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    const $form = $('form');
+    $form.submit(function (event) {
+      // 阻止原生 submit 行為
+      event.preventDefault();
+      if ($form.hasClass('needs-validation')) {
+        $form.removeClass('needs-validation');
+        $form.addClass('was-validated');
+      }
+  
+      if ($('.form-control:invalid').length === 0) {
+        // doLogin()
+        alert('登入囉:))))');
+        location.href = "/practise/jessie-shop/index.html";
+      }
+    });
+});
